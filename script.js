1f88f3a72c4aa7ce5fef0627a3c8f44bc02a4046
IsaiahCorn <isaiahcorn@outlook.com>
2017-12-12 12:23:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ function playEricLap(e){
 	document.getElementById("lap-top").play();
 }
 function play45(elem){
-	document.getElementById("45").play();
+	document.getElementById("1").play();
 }
+function playseminar(elem){
+	document.getElementById("2").play();
+}
+function play1light(elem){
+	document.getElementById("3").play();
+}
+
